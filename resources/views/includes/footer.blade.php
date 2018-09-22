58ed30fc34bc7d2776ceeb46ffca5981db353aca
André Lobo <loboandre@hotmail.com>
2018-09-22 10:41:21 -0300

    retornando ao projeto memoria

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,38 @@
+<!-- ***** Footer ***** -->
+    <footer class="footer-main">
+    	<div class="container">
+    		
+    		<div class="row">
+    			<div class="col-md-6">
+    				<a href="{{ url('/') }}">
+    					<img src="{{ asset('public/img/logo.png') }}" />
+					</a>
+					    		
+
+    			
+				</div><!-- ./End col-md-* -->
+
+				<div class="col-md-6">
+    				
+
+    			   <p class="margin-tp-xs">{{ $settings->description }}</p>
+    			   
+    			   
+				</div>
+  			
+    		
+    			
+    			
+    		</div><!-- ./End Row -->
+    	</div><!-- ./End Container -->
+    </footer><!-- ***** Footer ***** -->
+
+<footer class="subfooter">
+	<div class="container">
+	<div class="row">
+    			<div class="col-md-12 text-center padding-top-20">
+    				<p>RE9XTkxPQURFRCBGUk9NIENPREVMSVNULkND</p>
+    			</div><!-- ./End col-md-* -->
+	</div>
+</div>
+</footer>    
